7dab5095fcd20ce29f54f319c7736f037df0c5ab
Brooks Benson <brooksbenson01@gmail.com>
2018-04-03 13:07:51 -0600

    added connection state to nests, setup routeRequest

--- chapter11/crow-network.js
+++ chapter11/crow-network.js
@@ -41,7 +41,7 @@ function requestType(name, handler) {
     } catch (exception) {
       done(exception);
     }
-  })
+  });
 }
 
 //Ping request
@@ -67,7 +67,7 @@ everywhere(nest => {
   nest.state.gossip = [];
 });
 
-function sendGossip(nest, message, exceptFor) {
+function sendGossip(nest, message, exceptFor = null) {
   nest.state.gossip.push(message);
   for (let neighbor of nest.neighbors) {
     if (neighbor === exceptFor) continue;
@@ -79,3 +79,56 @@ requestType('gossip', (nest, message, source) => {
   if (nest.state.gossip.include(message)) return;
   sendGossip(nest, message, source);
 });
+
+
+requestType('connections', (nest, {name, neighbors}, source) => {
+  let {connections} = nest.state;
+  if (JSON.stringify(connections.get(name)) == 
+      JSON.stringify(neighbors)) return;
+  connections.set(name, neighbors);
+  broadcastConnections(nest, name, source);
+});
+
+function broadcastConnections(nest, name, exceptFor = null) {
+  for (let neighbor of nest.neighbors) {
+    if (neighbor == exceptFor) continue;
+    request(nest, neighbor, 'connections', {
+      name,
+      neighbors: nest.state.connections.get(name)
+    });
+  }
+}
+
+everywhere(nest => {
+  nest.state.connections = new Map;
+  nest.state.connections.set(nest.name, nest.neighbors);
+  broadcastConnections(nest, nest.name);
+});
+
+function findRoute(from, to, connections) {
+  let work = [{at: from, via: null}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, via} = work[i];
+    for (let next of connections.get(at)) {
+      if (next == to) return via;
+      if (!work.some(w => w.at == next)) {
+        work.push({at: next, via: via || next});
+      }
+    }
+  }
+  return null;
+}
+
+function routeRequest(nest, target, type, content) {
+  if (nest.neighbors.includes(target)) {
+    return request(nest, target, type, content);
+  } else {
+    let via = findRoute(nest.name, target, nest.state.connections);
+    if (!via) throw new Error(`No route to ${target}`);
+    return request(nest, via, 'route', {target, type, content});
+  }
+}
+
+requestType('route', (nest, {target, type, content}) => {
+  return routeRequest(nest, target, type, content);
+})
