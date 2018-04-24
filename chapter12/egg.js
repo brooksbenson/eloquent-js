1c9899b70aff6cbaf04e4914c9a9349f97e5e4c9
Brooks Benson <brooksbenson01@gmail.com>
2018-04-24 17:00:53 -0600

    added: egg programming syntax

--- chapter12/egg.js
+++ chapter12/egg.js
@@ -1,11 +1,24 @@
 /*
-  In Egg everything is an expression.
-    bindings - any characters ! \w ! keyword
-    numbers - sequence of digits
-    strings - sequence of chars wrapped in "" and doesn't contain "
-    applications
-      - parethesis after expression
-      - parenthesis contain arguments separated by commas
+  An example Egg program.
+
+  do(
+    define(x, 10),
+    if(
+      >(x, 5),
+      print("large"),
+      print("small")
+    )
+  )
+
+  Since Egg has no concept of a block,
+  an application called do is needed to
+  execute a sequence of actions.
+
+  The atomic elements of Egg:
+  - Strings
+  - Numbers
+  - Bindings
+  - Applications
 */
 
 // parsing an expression in egg
@@ -15,9 +28,9 @@ function parseExpression(program) {
   if (match = /^"([^"]*)"/.exec(program)) {
     expr = { type: 'value', value: match[1] };
   } else if (match = /^\d+\b/.exec(program)) {
-    expr = { type: 'value', value: ~~match[0] };
+    expr = { type: 'value', value: Number(match[0]) };
   } else if (match = /^[^\s(),"]+/.exec(program)) {
-    expr = { type: 'word', value: match[0] };
+    expr = { type: 'word', name: match[0] };
   } else {
     throw new SyntaxError('Unexpected syntax: ', program);
   }
@@ -32,7 +45,7 @@ function skipSpace(string) {
   return string.slice(first);
 }
 
-// checking if an expression is an application
+// parsing an application
 function parseApply(expr, program) {
   program = skipSpace(program);
   if (program[0] != '(') {
@@ -82,15 +95,120 @@ function evaluate(expr, scope) {
     case 'word':
       const { name } = expr
       if (name in scope) return scope[name];
-      throw new ReferenceError(`Undefined binding: ${expr.name}`);
+      throw new ReferenceError(`Undefined binding: ${name}`);
     case 'apply':
       const { operator, args } = expr;
       if (operator.type == 'word' && operator.name in specialForms) {
         return specialForms[operator.name](expr.args, scope);
       } else {
-        throw new TypeError('Applying a non-function');
+        let op = evaluate(operator, scope);
+        if (typeof op == 'function') {
+          return op(...args.map(arg => evaluate(arg, scope)));
+        } else {
+          throw new TypeError('Applying a non-function');
+        }
       } 
   } 
 }
 
-const test = evaluate(parse('+(4,4)'));
+// Adding specialForms
+specialForms.if = (args, scope) => {
+  if (args.length != 3) {
+    throw new SyntaxError('Wrong number of arguments to if');
+  } else if (evaluate(args[0], scope) !== false) {
+    return evaluate(args[1], scope);
+  } else {
+    return evaluate(args[2], scope);
+  }
+};
+
+specialForms.while = (args, scope) => {
+  if (args.length != 2) {
+    throw new SyntaxError('Wrong number of arguments to while');
+  }
+
+  while (evaluate(args[0], scope) !== false) {
+    evaluate(args[1], scope);
+  }
+
+  return false;
+};
+
+specialForms.do = (args, scope) => {
+  let value = false;
+  for (let arg of args) {
+    evaluate(arg, scope);
+  }
+  return value;
+};
+
+specialForms.define = (args, scope) => {
+  if (args.length != 2 || args[0].type != 'word') {
+    throw new SyntaxError('Incorrect use of define');
+  }
+  const value = evaluate(args[1], scope);
+  scope[args[0].name] = value;
+  return value;
+};
+
+// The Environment
+const topScope = Object.create(null);
+
+topScope.true = true;
+topScope.false = false;
+
+// Adding operators to The Environment
+for (let op of ['+', '-', '*', '/', '==', '<', '>', '/']) {
+  topScope[op] = new Function('a', 'b', `return a ${op} b`);
+}
+
+// Adding a way to print values
+topScope.print = value => {
+  console.log(value);
+  return value;
+};
+
+
+// Running Egg programs
+function run(program) {
+  return evaluate(parse(program), Object.create(topScope));
+}
+
+run(`
+do(define(total, 0),
+   define(count, 1),
+   while(<(count, 11),
+         do(define(total, +(total, count)),
+            define(count, +(count, 1)))),
+   print(total))
+`);
+
+// Functions in Egg
+specialForms.fun = (args, scope) => {
+  if (args.length === 0) {
+    throw new SyntaxError('Functions need a body');
+  }
+  let body = args[args.length - 1];
+  let params = args.slice(0, args.length - 1).map(expr => {
+    if (expr.type != 'word') {
+      throw new SyntaxError('Parameter names must be words');
+    }
+    return expr.name;
+  });
+
+  return function() {
+    if (arguments.length != params.length) {
+      throw new TypeError('Wrong number of arguments');
+    }
+    let localScope = Object.create(scope);
+    for (let i = 0; i < arguments.length; i++) {
+      localScope[params[i]] = arguments[i];
+    }
+    return evaluate(body, localScope);
+  }
+}
+
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10)))
+`);
--- chapter12/notes-chapter12.md
+++ chapter12/notes-chapter12.md
@@ -1,5 +1,16 @@
 # A Programming Language
 
+Building a programming language can be useful for domain specific problems. Regular Expressions, for example, provide a syntax for matching patterns in string data; a domain specific problem. The result of trying to match string data in JS may be more painful to look at and a lot more work to use than a regular expression. This is an example of how a new syntax can better solve domain specific problems.
+
 ## Parsing
 
 The most immediately visible part of a program is its syntax. A *parser* is program that reads a piece of text and produces a data structure that reflects the structure of the program contained in that text. If the text does not form a valid program, the parser points out the error.
+
+## Evaluating
+
+Evaluating is the process of running a program. In the beginning, the program is just some syntax. By parsing the syntax and forming a data structure, an evaluator can be built to run the program based on the data structure.
+
+## Compilation
+
+Compilation is the process of taking a program and converting it to another format. The new format should allow the program to be run more efficiently. Whenever a program is converted to another format it can be thought of as compilation.
+
