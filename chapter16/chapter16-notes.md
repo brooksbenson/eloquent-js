8f3dbb1c8d8272dac9da3b3ff40d0a0aec886329
Brooks Benson <brooksbenson01@gmail.com>
2018-05-23 15:49:49 -0600

    platform game: Level, GameState

---
+++ chapter16/chapter16-notes.md
@@ -0,0 +1,3 @@
+# Platform Game
+
+Platform games are games where players control an avatar that runs and jumps through a two-dimensional world. The avatar must avoid obstacles and do what is necessary to pass levels.
---
+++ chapter16/platform-game.js
@@ -0,0 +1,36 @@
+class Level {
+  constructor(plan) {
+    const rows = plan
+      .trim()
+      .split('\n')
+      .map(l => [...l]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.actors = []; // moving elements
+
+    this.rows = rows.map((row, y) => {
+      return row.map((char, x) => {
+        const type = levelChars[char];
+        if (typeof type == 'string') return type;
+        this.startActors.push(type.create(new Vec(x, y), char));
+        return 'empty';
+      });
+    });
+  }
+}
+
+class State {
+  constructor(level, actors, status) {
+    this.level = level;
+    this.actors = actors;
+    this.status = status;
+  }
+
+  static start(level) {
+    return new State(level, level.startActors, 'playing');
+  }
+
+  get player() {
+    return this.actors.find(a => a.type == 'player');
+  }
+}
